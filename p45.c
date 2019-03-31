5a4866689d25a32497b33942d0c7be44fca45404
Minchao Zhu <zhuminchao@aliyun.com>
2019-03-31 23:27:33 +0800

    p45: solution 3 greedy

--- p45.c
+++ p45.c
@@ -1,35 +1,53 @@
 #include <stdio.h>
-#include <string.h>
 
-int findMin(int* nums, int numsSize, int * min, int pos){
-    int val = 1, step = numsSize;
-    for(;val<=*(nums+pos);++val){
-        if(pos+val>=numsSize)
-            break;
-        if(step>*(min+pos+val))
-            step = *(min+pos+val);
+/*
+int jump(int* nums, int n) {
+    if (n == 1) return 0;
+    
+    int count = 1, i = 0, j = 0, mx_idx = 0;
+    
+    while (i+nums[i] < n-1) {
+        j = i; mx_idx = j;
+        while (j <= i+nums[i] && j < n) {
+            if (mx_idx + nums[mx_idx] < j + nums[j])
+                mx_idx = j;
+            j++;
+        }
+        
+        i = mx_idx;
+        count++;
+    }
+    
+    return count;
+}
+*/
+int jump(int* nums, int n) {
+    if(n==1)
+        return 0;
+    int pos = 0, step = 0;
+    int maxPos, j;
+    while(pos+nums[pos]<n-1){
+        j = pos+1;
+        maxPos = pos+1;
+        for(;j<(pos+nums[pos]+1)&&j<n;++j){
+            if(maxPos+nums[maxPos]<j+nums[j]){
+                maxPos = j;
+            }
+        }
+        pos = maxPos;
+        ++step;
     }
     return step+1;
-}
-
-int jump(int* nums, int numsSize) {
-    int min[numsSize];
-    min[numsSize-1] = 0;
-    int pos;
-    for(pos = numsSize-2;pos>=0;--pos){
-        *(min+pos) = findMin(nums,numsSize,min,pos);
-    }
-    return *min;
 }
 
 
 
 
 int main(void){
-    int A[39] = {5,6,4,4,6,9,4,4,7,4,4,8,2,6,8,1,5,9,6,5,2,7,9,7,9,6,9,4,1,6,8,8,4,4,2,0,3,8,5};
+    int A[20] = {9,8,2,2,0,2,2,0,4,1,5,7,9,6,6,0,6,5,0,5};
     int x;
     //int A[5] = {2,3,1,1,4};
-    x = jump(A,39);
+    x = jump(A,20);
     printf("%d\n",x);
 
     return 0;
