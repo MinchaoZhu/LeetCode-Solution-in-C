1ab80424c7a7af010cb196e1920ec0c0b924b760
Minchao Zhu <zhuminchao@aliyun.com>
2019-03-31 22:52:08 +0800

    p45: solution 2

--- p45.c
+++ p45.c
@@ -1,22 +1,25 @@
 #include <stdio.h>
+#include <string.h>
 
-
-int dfs(int* nums, int numsSize, int pos){
-    if(pos == numsSize-1)
-        return 0;
-    int i,min = numsSize,step;
-    for(i = 1;i<*(nums+pos)+1;++i){
-        if(pos+i>=numsSize)
+int findMin(int* nums, int numsSize, int * min, int pos){
+    int val = 1, step = numsSize;
+    for(;val<=*(nums+pos);++val){
+        if(pos+val>=numsSize)
             break;
-        step = dfs(nums,numsSize,pos+i);
-        if(step<min)
-            min = step;
+        if(step>*(min+pos+val))
+            step = *(min+pos+val);
     }
-    return min+1;
+    return step+1;
 }
 
 int jump(int* nums, int numsSize) {
-    return dfs(nums,numsSize,0);
+    int min[numsSize];
+    min[numsSize-1] = 0;
+    int pos;
+    for(pos = numsSize-2;pos>=0;--pos){
+        *(min+pos) = findMin(nums,numsSize,min,pos);
+    }
+    return *min;
 }
 
 
@@ -25,6 +28,7 @@ int jump(int* nums, int numsSize) {
 int main(void){
     int A[39] = {5,6,4,4,6,9,4,4,7,4,4,8,2,6,8,1,5,9,6,5,2,7,9,7,9,6,9,4,1,6,8,8,4,4,2,0,3,8,5};
     int x;
+    //int A[5] = {2,3,1,1,4};
     x = jump(A,39);
     printf("%d\n",x);
 
