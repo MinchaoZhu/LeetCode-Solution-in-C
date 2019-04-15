d294b645b063ca1a6ec0de82ad4f48e2a07bcd00
Minchao Zhu <zhuminchao@aliyun.com>
2019-04-15 20:46:50 +0800

    p67: solution 2

--- p67.c
+++ p67.c
@@ -1,62 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <string.h>
 
 
 char* addBinary(char* a, char* b) {
-    int carry = 0,i,j;
-    int len=0,la=0,lb=0;
-    char*aa=a,*bb=b;
-    while(*a){
-        ++la;
-        ++a;
+    int la = strlen(a), lb = strlen(b);
+    int lc = (la>lb?la:lb)+1;
+    int carry = 0;
+    char *new = (char*)malloc(sizeof(char)*(lc+1));
+    new[lc] = '\0';
+    while(la||lb){
+        if(la)carry+=a[--la]-'0';
+        if(lb)carry+=b[--lb]-'0';
+        new[--lc]=(carry&1)+'0';
+        carry = carry>>1;
     }
-    while(*b){
-        ++lb;
-        ++b;
+    new[0] = '1';
+    if(carry){
+        return new;
     }
-    len = la>lb?la:lb;
-    if(la<lb){
-        a=bb;
-        b=aa;
-        lb=la;
-    }
-    else{
-        a = aa;
-        b = bb;
-    }
-    aa = (char*)malloc(sizeof(char)*(len+1));
-    aa[len] = '\0';
-    j = len-1;
-    for(i = lb-1;i>=0;--i,--j){
-        aa[j] = a[j]+b[i]+carry-2*'0';
-        carry = aa[j]/2;
-        aa[j] = '0'+aa[j]%2;
-    }
-    for(;j>=0;--j){
-        aa[j] = a[j]+carry-'0';
-        carry = aa[j]/2;
-        aa[j] = '0'+aa[j]%2;
-    }
-    if(!carry)return aa;
-    else{
-        bb = (char*)malloc(sizeof(char)*(len+2));
-        bb[0] = 1+'0';
-        bb[len+1] = '\0';
-        for(i = 1;i<len+1;++i){
-            bb[i] = aa[i-1];
-        }
-        return bb;
-    }
-
+    else
+        return new+1;
 }
 
 
 
 
 int main(void){
-    char* a = "1";
-    char* b = "111";
+    char* a = "10";
+    char* b = "101";
     char* s;
     s = addBinary(a,b);
     printf("%s\n",s);
