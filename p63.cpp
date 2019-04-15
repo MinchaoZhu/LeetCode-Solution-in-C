d24ac317eef84e56221eb40ee979a6349460b09b
Minchao Zhu <zhuminchao@aliyun.com>
2019-04-15 11:36:14 +0800

    p63: solution 2 my solution

--- p63.cpp
+++ p63.cpp
@@ -5,32 +5,53 @@ using namespace std;
 class Solution {
 public:
     int uniquePathsWithObstacles(vector<vector<int>>& table0) {
-        int n = table0.size(), m = table0[0].size(),i;
-        long obstacleGrid[n][m];
-        for(i = 0;i<n;++i){
-            for(int j = 0;j<m;++j){
-                obstacleGrid[i][j] = table0[i][j];
+        int m = table0.size(), n = table0[0].size(),i;
+        long table[m][n];
+        for(i = 0;i<m;++i){
+            for(int j = 0;j<n;++j){
+                table[i][j] = table0[i][j];
             }
         }
-        if(m==0 || obstacleGrid[0][0] == 1 || obstacleGrid[n-1][m-1] == 1) return 0;
-        
-        obstacleGrid[n-1][m-1] = 1;
-        for(int j=m-1;j>=0;j--) { 
-            for(int i=(j==m-1)?n-2:n-1; i>=0; i--) {
-                if(--obstacleGrid[i][j] == -1) {
-				   // unary ? takes care of boundary conditions.
-                    obstacleGrid[i][j] = (i==n-1 ? 0 : obstacleGrid[i+1][j]) + 
-                                         (j==m-1 ? 0 : obstacleGrid[i][j+1]);
+        if(table[m-1][n-1]==0)
+                table[m-1][n-1] = 1;
+        else return 0;
+        for(i = m-2;i>=0;--i){
+                if(table[i][n-1]!=1)
+                        table[i][n-1] = table[i+1][n-1];
+                else
+                        table[i][n-1] = 0;
+        }
+        for(i = n-2;i>=0;--i){
+                if(table[m-1][i]!=1)
+                        table[m-1][i] = table[m-1][i+1];
+                else
+                        table[m-1][i] = 0;
+        }
+        if(m==1||n==1)return table[0][0];
+        --m;--n;
+        while(m>0&&n>0){
+                for(i = m-1;i>=0;--i){
+                        if(table[i][n-1]!=1)
+                                table[i][n-1] = table[i+1][n-1]+table[i][n];
+                        else
+                                table[i][n-1] = 0;        
                 }
-            }
+                for(i = n-2;i>=0;--i){
+                        if(table[m-1][i]!=1)
+                                table[m-1][i] = table[m][i]+table[m-1][i+1];
+                        else
+                                table[m-1][i] = 0;  
+                }
+                --m;--n;
         }
-        
-        return obstacleGrid[0][0];
+    return table[0][0];
     }
 };
 
 int main(void){
-    vector<vector<int>> t = {{0,0},{0,0},{0,0},{0,0},{0,0},{0,0},{1,0},{0,0},{0,0},{0,0},{0,0},{0,0},{1,0},{0,0},{0,0},{0,0},{0,0},{0,1},{0,0},{0,0},{1,0},{0,0},{0,0},{0,1},{0,0},{0,0},{0,0},{0,0},{0,0},{0,0},{0,0},{0,1},{0,0},{0,0},{0,0},{0,0},{1,0},{0,0},{0,0},{0,0},{0,0}};
+    vector<vector<int>> t = {{0,1},{0,0},{0,0},{0,0},{0,0},{1,0},{0,0},{0,0}};
+    
+    //vector<vector<int>> t = {{0,0},{0,0},{0,0},{0,0},{0,0},{0,0},{1,0},{0,0},{0,0},{0,0},{0,0},{0,0},{1,0},{0,0},{0,0},{0,0},{0,0},{0,1},{0,0}};
     int i;
     Solution s;
     i = s.uniquePathsWithObstacles(t);
