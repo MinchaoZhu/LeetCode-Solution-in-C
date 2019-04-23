22a58235172ef768ed5b637569bce822697dfb84
Minchao Zhu <zhuminchao@aliyun.com>
2019-04-23 13:17:34 +0800

    p77: solution 1

---
+++ p77.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+class Solution {
+public:
+    vector<vector<int>> combine(int n, int k) {
+        vector<vector<int>> result;
+        vector<int> temp;
+        recur(result,temp,n,1,k);
+        return result;
+    }   
+
+private:
+    void recur(vector<vector<int>>& result, vector<int>& temp, int n, int start, int k){
+        if(!k){
+            result.push_back(temp);
+            return;
+        }
+        else{
+            for(int cur = start;cur<n+1;++cur){
+                temp.push_back(cur);
+                recur(result,temp,n,cur+1,k-1);
+                temp.pop_back();
+            }
+        }
+    }
+};
+
+
+
+int main(void){
+    vector<vector<int>> r;
+
+    return 0;
+}
