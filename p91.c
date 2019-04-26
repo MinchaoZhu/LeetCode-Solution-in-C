64c9b1212f27e24bf243a487080682d20f2aa191
Minchao Zhu <zhuminchao@aliyun.com>
2019-04-27 07:25:16 +0800

    p91: solution 2, faster

--- p91.c
+++ p91.c
@@ -1,29 +1,26 @@
 #include <stdio.h>
 #include <string.h>
 
-int count(char * s, int start, int len){
-    if(len<=1)return s[start]=='0'?0:1;
-    else{
-        if(s[start]=='0')return 0;
-        if((s[start]=='1'||s[start]=='2')&&(len>1&&s[start+1]=='0')) return count(s,start+2,len-2);
-        if(s[start]=='2'&&s[start+1]>'6'||s[start]>'2')return count(s,start+1,len-1);
-        else return count(s,start+1,len-1)+count(s,start+2,len-2);
-    }
-}
-
 int numDecodings(char * s){
-    int i = 0;
-    for(;i<strlen(s);++i){
-        if(s[i]=='0'&&(i==0||(i>0&&s[i-1]>'2')||(i>0&&s[i-1]=='0'))) return 0;
+    int len = strlen(s);
+    int count[len];
+    int i = len-3;
+    count[len-1] = s[len-1]=='0'?0:1;
+    if(len>1){
+        if(s[len-2]=='1'||(s[len-2]=='2'&&s[len-1]<='6'))count[len-2] = 1+count[len-1];
+        else if(s[len-2] == '0')count[len-2] = 0;
+        else count[len-2] = count[len-1];
     }
-    return count(s,0,strlen(s));
+    for(;i>=0;--i){
+        if(s[i]=='1'||(s[i]=='2'&&s[i+1]<='6'))count[i] = count[i+1]+count[i+2];
+        else if(s[i] == '0')count[i] = 0;
+        else count[i] = count[i+1];        
+    }
+    return count[0];
 }
-
-
-
 
 int main(void){
-    char * s = "110";
+    char * s = "130";
     int t;
     t = numDecodings(s);
     printf("%d\n",t);
