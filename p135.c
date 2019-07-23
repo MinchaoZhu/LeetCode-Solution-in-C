0a4820a229165e748823195f8cb0483e375d40c3
Minchao Zhu <zhuminchao@aliyun.com>
2019-07-23 20:16:58 +0800

    p135: solution 2

--- p135.c
+++ p135.c
@@ -6,15 +6,14 @@ int candy(int* ratings, int ratingsSize){
     for(int i = 0;i<ratingsSize;++i)candies[i] = 1;
     if(ratingsSize>1&&ratings[0]>ratings[1])candies[0] = candies[1] + 1;
     for(int i = 1;i<ratingsSize-1;++i){
-        if(ratings[i]>ratings[i-1]) candies[i] = candies[i-1] + 1;
-        if(ratings[i]>ratings[i+1]) candies[i] = candies[i]>candies[i+1]?candies[i]:candies[i+1]+1;
-        int j = i;
-        while(j>=1&&ratings[j]<ratings[j-1]){
-            if(!(candies[j]<candies[j-1])) candies[j-1] = candies[j] + 1;
-            --j;
-        }
+        if(ratings[i]>ratings[i-1]) candies[i] = candies[i]>(candies[i-1] + 1)?candies[i]:(candies[i-1]+1);;
     }
+    //printArr(candies,ratingsSize);
     if(ratingsSize>1&&ratings[ratingsSize-1]>ratings[ratingsSize-2]) candies[ratingsSize-1] = candies[ratingsSize-2] + 1;
+    for(int i = ratingsSize-2;i>0;--i){
+        if(ratings[i]>ratings[i+1]) candies[i] = candies[i]>(candies[i+1] + 1)?candies[i]:(candies[i+1]+1);
+    }
+    if(ratingsSize>1&&ratings[0]>ratings[1])candies[0] = candies[1] + 1;
     for(int i = 0;i<ratingsSize;++i)result+=candies[i];
     //printArr(candies,ratingsSize);
     return result;
@@ -23,12 +22,13 @@ int candy(int* ratings, int ratingsSize){
 void printArr(int * arr, int size){
     for(int i = 0;i<size;++i)
         printf("%d \n",arr[i]);
+    printf("\n");
 }
 
 int main(void){
-    int arr[3] = {3,2,1};
+    int arr[6] = {1,2,3,3,3,2};
     int result;
-    result = candy(arr,3);
-
+    result = candy(arr,6);
+    printf("result = %d\n",result);
     return 0;
 }
