ae9ec07ce7c143dff2fcb6a83707ac80f9e64468
Minchao Zhu <zhuminchao@aliyun.com>
2019-07-24 20:55:44 +0800

    p142: solution 2

--- p142.cpp
+++ p142.cpp
@@ -18,13 +18,21 @@ struct ListNode {
 class Solution {
 public:
     ListNode *detectCycle(ListNode *head) {
-        std::set<ListNode*> nodeSet;
-        while(head){
-            if(nodeSet.find(head)==nodeSet.end()){
-                nodeSet.insert(head);
-                head = head->next;
+        ListNode* p1=head, *p2=head;
+        if(head){
+            while(p1&&p2&&p2->next){
+                p1 = p1->next;
+                p2 = p2->next->next;
+                if(p1==p2){
+                    p1 = head;
+                    while(p1!=p2){
+                        p1 = p1->next;
+                        p2 = p2->next;
+                    }
+                    return p1;
+                }
             }
-            else return head;
+            return NULL;
         }
         return NULL;
     }
