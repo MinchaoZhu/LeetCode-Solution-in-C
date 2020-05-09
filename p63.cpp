200d6ec94b0a0d377e246a03fd358020e420a7b7
Minchao Zhu <zhuminchao@aliyun.com>
2020-05-09 15:45:23 -0500

    p63: dp

--- p63.cpp
+++ p63.cpp
@@ -4,48 +4,69 @@ using namespace std;
 
 class Solution {
 public:
-    int uniquePathsWithObstacles(vector<vector<int>>& table0) {
-        int m = table0.size(), n = table0[0].size(),i;
-        long table[m][n];
-        for(i = 0;i<m;++i){
-            for(int j = 0;j<n;++j){
-                table[i][j] = table0[i][j];
+//     int uniquePathsWithObstacles(vector<vector<int>>& table0) {
+//         int m = table0.size(), n = table0[0].size(),i;
+//         long table[m][n];
+//         for(i = 0;i<m;++i){
+//             for(int j = 0;j<n;++j){
+//                 table[i][j] = table0[i][j];
+//             }
+//         }
+//         if(table[m-1][n-1]==0)
+//                 table[m-1][n-1] = 1;
+//         else return 0;
+//         for(i = m-2;i>=0;--i){
+//                 if(table[i][n-1]!=1)
+//                         table[i][n-1] = table[i+1][n-1];
+//                 else
+//                         table[i][n-1] = 0;
+//         }
+//         for(i = n-2;i>=0;--i){
+//                 if(table[m-1][i]!=1)
+//                         table[m-1][i] = table[m-1][i+1];
+//                 else
+//                         table[m-1][i] = 0;
+//         }
+//         if(m==1||n==1)return table[0][0];
+//         --m;--n;
+//         while(m>0&&n>0){
+//                 for(i = m-1;i>=0;--i){
+//                         if(table[i][n-1]!=1)
+//                                 table[i][n-1] = table[i+1][n-1]+table[i][n];
+//                         else
+//                                 table[i][n-1] = 0;        
+//                 }
+//                 for(i = n-2;i>=0;--i){
+//                         if(table[m-1][i]!=1)
+//                                 table[m-1][i] = table[m][i]+table[m-1][i+1];
+//                         else
+//                                 table[m-1][i] = 0;  
+//                 }
+//                 --m;--n;
+//         }
+//     return table[0][0];
+//     }
+
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        int m = obstacleGrid.size();
+        int n = obstacleGrid[0].size();
+        int dp[m][n];
+        dp[0][0] = !obstacleGrid[0][0];
+        for(int i = 1; i<m; ++i){
+           dp[i][0] = !obstacleGrid[i][0]&&dp[i-1][0];     
+        }
+        for(int j = 0; j<n; ++j){
+           dp[0][j] = !obstacleGrid[0][j]&&dp[0][j];  
+        }
+        for(int i = 1; i<m; ++i){
+            for(int j = 1; j<n; ++j){
+                dp[i][j] = obstacleGrid[i][j]==1?0:dp[i-1][j]+dp[i][j-1];
             }
         }
-        if(table[m-1][n-1]==0)
-                table[m-1][n-1] = 1;
-        else return 0;
-        for(i = m-2;i>=0;--i){
-                if(table[i][n-1]!=1)
-                        table[i][n-1] = table[i+1][n-1];
-                else
-                        table[i][n-1] = 0;
-        }
-        for(i = n-2;i>=0;--i){
-                if(table[m-1][i]!=1)
-                        table[m-1][i] = table[m-1][i+1];
-                else
-                        table[m-1][i] = 0;
-        }
-        if(m==1||n==1)return table[0][0];
-        --m;--n;
-        while(m>0&&n>0){
-                for(i = m-1;i>=0;--i){
-                        if(table[i][n-1]!=1)
-                                table[i][n-1] = table[i+1][n-1]+table[i][n];
-                        else
-                                table[i][n-1] = 0;        
-                }
-                for(i = n-2;i>=0;--i){
-                        if(table[m-1][i]!=1)
-                                table[m-1][i] = table[m][i]+table[m-1][i+1];
-                        else
-                                table[m-1][i] = 0;  
-                }
-                --m;--n;
-        }
-    return table[0][0];
+        return dp[m-1][n-1];
     }
+
+
 };
 
 int main(void){
