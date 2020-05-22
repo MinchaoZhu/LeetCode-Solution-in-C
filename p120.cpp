921245875c0ec5c1ebf6bf75144aa75261b376d9
Minchao Zhu <zhuminchao@aliyun.com>
2020-05-22 15:48:13 -0500

    p120: dp

--- p120.cpp
+++ p120.cpp
@@ -28,6 +28,29 @@ public:
     }
 };
 
+// [C] DP
+
+// int minimumTotal(int** triangle, int triangleSize, int* triangleColSize){
+    // if(triangleSize>0){
+        // int dp[triangleSize][triangleSize];
+        // dp[0][0] = triangle[0][0];
+        // for(int i = 1; i < triangleSize; ++i){
+            // dp[i][0] = dp[i-1][0] + triangle[i][0];
+            // dp[i][i] = dp[i-1][i-1] + triangle[i][i];
+            // for(int j = 1; j < i; ++j){
+                // int tmp1 = dp[i-1][j-1], tmp2 = dp[i-1][j];
+                // dp[i][j] = triangle[i][j] + (tmp1<tmp2?tmp1:tmp2);
+            // }
+        // }
+        // int min = dp[triangleSize-1][0];
+        // for(int j = 1; j<triangleSize; ++j){
+            // min = min<dp[triangleSize-1][j]?min:dp[triangleSize-1][j];
+        // }
+        // return min;
+    // }
+    // else return 0;
+// }
+
 
 int main(void){
     vector<vector<int>> t = {{2},{3,4},{6,5,7},{4,1,8,3}};
