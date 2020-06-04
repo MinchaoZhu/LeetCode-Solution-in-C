d3be35a5cf1d52e41958bde9cedd9d37729032d1
Minchao Zhu <zhuminchao@aliyun.com>
2020-06-04 01:53:52 -0500

    p42: dp & double pointer

--- p42.c
+++ p42.c
@@ -1,6 +1,57 @@
 #include <stdio.h>
 
+// class Solution {
+// public:
+//     int trap(vector<int>& height) {
+//         int l = 0, r = height.size()-1;
+//         int ml = 0, mr = 0, res = 0;
+//         while (l < r) {
+//             if (height[l] < height[r]) {
+//                 ml = max(ml, height[l]);
+//                 res += ml - height[l];
+//                 ++l;
+//             } else {
+//                 mr = max(mr, height[r]);
+//                 res += mr - height[r];
+//                 --r;
+//             }
+//         }
+//         return res;
+//     }
+// };
 
+// class Solution {
+// public:
+//     int trap(vector<int>& height) {
+//         int size = height.size();
+//         if(size>2){
+//             int left[size];
+//             int right[size];
+            
+//             int tmp = 0;
+//             for(int i = 0; i<size; ++i){
+//                 left[i] = tmp;
+//                 tmp = tmp>height[i]?tmp:height[i];
+//             }
+            
+//             tmp = 0;
+//             for(int i = size-1; i>=0; --i){
+//                 right[i] = tmp;
+//                 tmp = tmp>height[i]?tmp:height[i];
+//             }
+            
+//             tmp = 0;
+//             for(int i = 0; i<size; ++i){
+//                 int h = min(left[i], right[i]);
+//                 tmp += h>height[i]?h-height[i]:0;
+//             }
+            
+//             return tmp;
+            
+//         }
+//         else return 0;
+//     }
+// };
 
 
 int trap(int* h, int n) {
