64cbdd3ae634e9c8788a89e967b654453d1b88c5
Minchao Zhu <zhuminchao@aliyun.com>
2020-06-05 23:06:49 -0500

    p23: priority queue

--- p23.c
+++ p23.c
@@ -1,6 +1,64 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+
+
+
+
+
+
+// /**
+//  * Definition for singly-linked list.
+//  * struct ListNode {
+//  *     int val;
+//  *     ListNode *next;
+//  *     ListNode() : val(0), next(nullptr) {}
+//  *     ListNode(int x) : val(x), next(nullptr) {}
+//  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+//  * };
+//  */
+// class Solution {
+// public:
+//     ListNode* mergeKLists(vector<ListNode*>& lists) {
+        
+//         auto cmp = [](ListNode* n1, ListNode* n2) { return n1->val > n2->val; };
+//         unordered_map<ListNode*, int> m; 
+//         priority_queue<ListNode*, vector<ListNode*>, decltype(cmp)> heap(cmp);
+//         ListNode* head = new ListNode();
+//         ListNode* current = head;
+//         int k = lists.size();
+        
+//         for(int i = 0; i<k; ++i){
+//             if(lists[i]){
+//                 m[lists[i]] =  i;
+//                 heap.push(lists[i]);
+//                 lists[i] = lists[i] -> next;
+//             }
+//         }
+        
+//         while(!heap.empty()){
+//             ListNode* tmp = heap.top();
+//             heap.pop();
+//             current -> next = tmp;
+//             current = tmp;
+//             ListNode* nextNode = lists[m[tmp]];
+//             if(nextNode){
+//                 m[nextNode] = m[tmp];
+//                 heap.push(nextNode);
+//                 lists[m[tmp]] = lists[m[tmp]] -> next;
+//             }
+//         }
+//         current = head -> next;
+//         delete(head);
+//         return current;
+//     }
+    
+// };
+
+
+
+
+
 /**
  * Definition for singly-linked list.
 struct ListNode {
