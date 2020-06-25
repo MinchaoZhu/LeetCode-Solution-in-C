a94bd1e4fe123ed592551b71d8dcd0d385a97941
Minchao Zhu <zhuminchao@aliyun.com>
2020-06-25 17:20:36 -0500

    p287 solution 2

--- p287.cpp
+++ p287.cpp
@@ -3,15 +3,29 @@ public:
     int findDuplicate(vector<int>& nums) {
         int size = nums.size();
         for(int i = 0; i < size; ++i){
-            while(nums[i] != i + 1){
-                if(nums[nums[i]-1] == nums[i])
-                    return nums[i];
-                int tmp = nums[i];
-                nums[i] = nums[nums[i]-1];
-                nums[tmp-1] = tmp;
-            }
+            int val = abs(nums[i]);
+            if(nums[val] < 0) return val;
+            nums[val] = -nums[val];
         }
-        
         return -1;
     }
 };
+
+// class Solution {
+// public:
+//     int findDuplicate(vector<int>& nums) {
+//         int size = nums.size();
+//         for(int i = 0; i < size; ++i){
+//             while(nums[i] != i + 1){
+//                 if(nums[nums[i]-1] == nums[i])
+//                     return nums[i];
+//                 int tmp = nums[i];
+//                 nums[i] = nums[nums[i]-1];
+//                 nums[tmp-1] = tmp;
+//             }
+//         }
+        
+//         return -1;
+//     }
+// };
+
