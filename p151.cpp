9064ad756ffcece5f8a2a372ff062efef9a88b41
Minchao Zhu <zhuminchao@aliyun.com>
2020-07-15 23:26:39 -0500

    p151

--- p151.cpp
+++ p151.cpp
@@ -4,6 +4,35 @@
 using namespace std;
 
 
+
+// class Solution {
+// public:
+//     string reverseWords(string s) {
+//         vector<string> v = split(s, " ");
+//         if(v.size() <= 0) return "";
+            
+//         string result = v[v.size()-1];
+//         for(int i = v.size() - 2; i >=0; --i) {
+//             result = result + " " + v[i];
+//         }
+//         return result;
+//     }
+
+// private:
+//     std::vector<std::string> split(std::string str,std::string sep){
+//         char* cstr=const_cast<char*>(str.c_str());
+//         char* current;
+//         std::vector<std::string> arr;
+//         current=strtok(cstr,sep.c_str());
+//         while(current!=NULL){
+//             arr.push_back(current);
+//             current=strtok(NULL,sep.c_str());
+//         }
+//         return arr;
+//     }
+    
+// };
+
 class Solution {
 public:
     string reverseWords(string s) {
